d6794aeab5ae40cefc783ecce59eedb1750f33ac
soejinxx-lang <soejinxx@gmail.com>
2025-12-26 01:00:11 +0900

    Create route.ts

---
+++ app/api/novels/[id]/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { id: string } }
+) {
+  return NextResponse.json({
+    id: params.id,
+    title: "",
+    description: "",
+  });
+}
