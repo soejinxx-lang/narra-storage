d3fbd16e0171ed2046b6b9f64b4ac7c003414664
soejinxx-lang <soejinxx@gmail.com>
2025-12-26 02:26:42 +0900

    Update route.ts

--- app/api/novels/[id]/episodes/route.ts
+++ app/api/novels/[id]/episodes/route.ts
@@ -1,11 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  _req: Request,
-  { params }: { params: { id: string } }
+  _req: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }
 ) {
+  const { id } = await params;
+
   return NextResponse.json({
-    novelId: params.id,
+    novelId: id,
     episodes: [],
   });
 }
