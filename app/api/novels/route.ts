74a15214981603482399a51c4c71a76a5104f25b
soejinxx-lang <soejinxx@gmail.com>
2025-12-26 02:31:09 +0900

    Update route.ts

--- app/api/novels/route.ts
+++ app/api/novels/route.ts
@@ -1,6 +1,8 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(
+  _req: NextRequest
+) {
   return NextResponse.json({
     novels: [
       {
