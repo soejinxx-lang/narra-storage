bf04954eb467fe75f43faafa989d1e465a726b6d
soejinxx-lang <soejinxx@gmail.com>
2026-01-28 15:29:20 +0900

    Update route.ts

--- app/api/novels/[id]/episodes/translations-status/route.ts
+++ app/api/novels/[id]/episodes/translations-status/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
 import db, { initDb } from "../../../../../db";
+
+export const dynamic = "force-dynamic";
 
 export async function GET(
   _req: NextRequest,
